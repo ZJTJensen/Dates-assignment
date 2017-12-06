c9a0169bb46feb27a46d8d407757157758c01480
Zachary Jensen <ZJTJensen@yahoo.com>
2017-12-06 08:29:41 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Timeouts } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+   today = new Date();
+   lastTimeZoneSeleted = null;
+   onTZButtonClick(timezone){
+      this.today = new Date();
+      if (timezone == 'CST'){
+      this.today.setHours(this.today.getHours() - 1);
+      }
+      else if (timezone == 'MST'){
+      this.today.setHours(this.today.getHours() - 2);
+      }
+      else if(timezone == 'PST'){
+      this.today.setHours(this.today.getHours() - 3);
+      }
+
+      this.lastTimeZoneSeleted = timezone;
+      console.log(this.lastTimeZoneSeleted)
+   }
+  // pst(time){
+  //   time = 'PST'
+  // }
+  // mst(time){
+  //   time = 'MST'
+  // }
+  // cst(time){
+  //   time = 'CST'
+  // }
+  // est(time){
+  //   time = 'EST'
+  // }
+  // clear(time){
+
+  // }
 }
